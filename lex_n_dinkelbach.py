54cc27af7e7a9ec06ac4c05bebb4963e202f5007
Happynood <cthjd24@yandex.ru>
2024-04-01 19:06:59 +0300

    delay;)

--- lex_n_dinkelbach.py
+++ lex_n_dinkelbach.py
@@ -3,6 +3,7 @@ from scipy import spatial
 import copy
 import numpy as np
 from fractions import Fraction
+#import time
 
 def rev(lst):
     return [ -i for i in lst ] 
@@ -12,9 +13,22 @@ def check_stb_more_zero(a,x):
       if a[i][x]<0: return False
 
     return True
+def beauty_print_simplex_table(a):
+   df = pd.DataFrame(data = a)
+   print(df)
 
+def search_main_basis(a):
+   num_of_vectors_which_are_basis = []
+   for i in range(1,len(a[0])-1):
+       count_of_zeros = 0
+       for j in range(0,len(a)-1):
+          if a[j][i]==0:count_of_zeros+=1
+       if count_of_zeros==len(a)-2 and len(num_of_vectors_which_are_basis)<len(a)-1: num_of_vectors_which_are_basis.append(i-1)
+   return num_of_vectors_which_are_basis
+
+          
 def iter_lex(a):
-  max_x = 0
+  max_x = 1
   max_y = 0
   while a[len(a)-1][max_x]>=0:
     max_x+=1
@@ -22,7 +36,7 @@ def iter_lex(a):
       #print('Оптимальное решение найдено')
       return a,True
   for x in range(max_x,len(a[0])):
-    if abs(a[len(a)-1][x])>abs(a[len(a)-1][max_x]) and a[len(a)-1][x]<0  and  check_stb_more_zero(a,x) : 
+    if x!=0 and abs(a[len(a)-1][x])>abs(a[len(a)-1][max_x]) and a[len(a)-1][x]<0  and  check_stb_more_zero(a,x) : 
       max_x = x
   while a[max_y][max_x]<=0:
     max_y+=1
@@ -82,15 +96,19 @@ def alg_dinkelvach(P,D,A,b,symb):
     
     lambda_alg = Fraction()
     count_lambda_iter = 0
-    crit_count_lambda_iter = 2
-    while (lambda_alg!=0 and count_lambda_iter<crit_count_lambda_iter) or count_lambda_iter==0:
+    crit_count_lambda_iter = 1000
+    F = 1
+    while (lambda_alg!=0 and F!=0 and count_lambda_iter<crit_count_lambda_iter) or count_lambda_iter==0:
+        print('--------------ITERATION ',count_lambda_iter,'------------------')
+       
         lambda_alg = Fraction(insert_x_in_P(dbr),insert_x_in_D(dbr))
+        print(count_lambda_iter,' lambda',lambda_alg)
         new_C = P-lambda_alg*D
-
+        print('Mission: ',new_C)
         lin_prog = A.tolist()
 
         lin_prog.insert(len(A),rev(new_C[:-1]))
-        lin_prog[len(A)].insert(0,Fraction(0))
+        lin_prog[len(A)].insert(0,Fraction(new_C[len(new_C)-1]))
         for x in range(len(A)):
             lin_prog[x].insert(0,b[x])
         if symb =="<=":
@@ -98,21 +116,33 @@ def alg_dinkelvach(P,D,A,b,symb):
             for l in range(len(lin_prog)):
                 for k in range(len(lin_prog)-1):
                     lin_prog[l].insert(count_stb+k,Fraction(0))
-            for l in range(count_stb-1,len(lin_prog[0])-1):    
-                lin_prog[count_stb-l][l]=Fraction(1)
-
-        print(lin_prog)
+            strr = 0
+            for l in range(count_stb,len(lin_prog[0])):    
+                lin_prog[strr][l]=Fraction(1)
+                strr+=1
+        print('Task of lin prog')
+        beauty_print_simplex_table(lin_prog)
         solved_lin_prog_task = False
         crit_count_lin_prog = 1000
         count_lin_prog = 0
         while solved_lin_prog_task==False and count_lin_prog <crit_count_lin_prog:
            lin_prog,solved_lin_prog_task = iter_lex(lin_prog)
            count_lin_prog+=1
+        nums_of_basis = search_main_basis(lin_prog)
         for copy_dbr in range(len(dbr)):
-           dbr[copy_dbr]=lin_prog[0][copy_dbr]
+           if copy_dbr in nums_of_basis:
+              dbr[copy_dbr]=lin_prog[copy_dbr][0]
+           else:
+              dbr[copy_dbr]=0
         count_lambda_iter+=1
-        print(count_lambda_iter)
-        print (dbr)
+        F = lin_prog[len(lin_prog)-1][0]
+        print('End iteration:')
+        beauty_print_simplex_table(lin_prog)
+        print('Num of vec in basis',nums_of_basis)
+        print ('Basis solution', dbr)
+        print('F = ',F, '\n\n')
+        #time.sleep(5)
+    return dbr
 
 
 alg_dinkelvach(np.array([Fraction(5),Fraction(2),Fraction(3)]),np.array([Fraction(3),Fraction(1),Fraction(2)]),np.array([[Fraction(6),Fraction(4)],[Fraction(3),Fraction(5)]]),np.array([Fraction(25),Fraction(20)]),"<=")
