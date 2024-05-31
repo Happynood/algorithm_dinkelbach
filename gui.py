faa4afcf39181fde65ad3c4619b34011253bd327
Happynood <cthjd24@yandex.ru>
2024-05-31 16:53:03 +0300

    ver31.05

--- gui.py
+++ gui.py
@@ -3,13 +3,75 @@ import time
 import copy
 from PyQt5 import QtWidgets, QtGui
 from main_page import Ui_Dialog
-from lex_n_dinkelbach import rev,alg_dinkelbach
-from lex_n_dinkelbach import alg_dinkelbach1
+from lex_n_dinkelbach import rev,alg_dinkelbach,alg_dinkelbach_with_no_output
+from lex_n_dinkelbach import iter_lex
 from fractions import Fraction
 import numpy as np
 
-pashalko = ''
-counter = 5
+def double_phaz_sm(A,b):
+    task_of_test = [Fraction(1)]*(len(A[0])+1)
+    lin_prog =copy.deepcopy(A)
+    lin_prog.insert(len(A),rev(task_of_test[:-1]))
+    for x in range(len(A)):
+        lin_prog[x].insert(0,b[x])
+    
+    lin_prog[len(A)].insert(0,Fraction(0))  
+
+    count_stb = len(lin_prog[0])
+    for l in range(len(lin_prog)):
+        for k in range(len(lin_prog)-1):
+            lin_prog[l].insert(count_stb+k,Fraction(0))
+    strr = 0
+    for l in range(count_stb,len(lin_prog[0])):    
+        lin_prog[strr][l]=Fraction(1)
+        strr+=1
+    count_stb = len(lin_prog[0])
+    for l in range(len(lin_prog)):
+        for k in range(len(lin_prog)-1):
+            lin_prog[l].insert(count_stb+k,Fraction(0))
+    strr = 0
+    for l in range(count_stb,len(lin_prog[0])):    
+        lin_prog[strr][l]=Fraction(1)
+        strr+=1 
+     
+    solved_lin_prog_task =False
+    while lin_prog != [] and solved_lin_prog_task ==False:
+        lin_prog,solved_lin_prog_task = iter_lex(lin_prog)
+    #Критерий отсутствия решения
+    if lin_prog[len(lin_prog)-1][0]<0 or lin_prog == [] : return False
+    return True
+
+def output_error_log(message,is_check,path,save):
+        if is_check and save:
+            log = open(path + '/log.txt', 'a', encoding='utf-8')
+            print(message,file=log)
+        if is_check and save==False:
+            log = open('./log.txt', 'a', encoding='utf-8')
+            print(message,file=log)
+
+def inf_simplex(A,b):
+    task_of_test = [Fraction(1)]*(len(A[0])+1)
+    lin_prog =copy.deepcopy(A)
+    lin_prog.insert(len(A),rev(task_of_test[:-1]))
+    for x in range(len(A)):
+        lin_prog[x].insert(0,b[x])
+    
+    lin_prog[len(A)].insert(0,Fraction(0))  
+
+    count_stb = len(lin_prog[0])
+    for l in range(len(lin_prog)):
+        for k in range(len(lin_prog)-1):
+            lin_prog[l].insert(count_stb+k,Fraction(0))
+    strr = 0
+    for l in range(count_stb,len(lin_prog[0])):    
+        lin_prog[strr][l]=Fraction(1)
+        strr+=1 
+    solved_lin_prog_task =False
+    while lin_prog != [] and solved_lin_prog_task ==False:
+        lin_prog,solved_lin_prog_task = iter_lex(lin_prog)
+    #Критерий отсутствия решения
+    if lin_prog == [] and solved_lin_prog_task ==False: return False
+    return True
 class mywindow(QtWidgets.QMainWindow):
  
     def __init__(self):
@@ -27,46 +89,28 @@ class mywindow(QtWidgets.QMainWindow):
         self.save = True
     
     def btnClicked(self):
-        global pashalko
-        global counter
+      
         self.ui.textBrowser.clear()
         # Если не использовать, то часть текста исчезнет.
         task = self.ui.textEdit.toPlainText()
-        if task == pashalko:
-            counter -= 1
-        else:
-            pashalko = task
-            counter = 5
-        if -10<= counter <= 0:
-            self.ui.textBrowser.append("Уважаемый пользователь!\nХватит вводить одинаковые\nданные! Ответ не изменится!")
-            return
-        elif -25<= counter < -10:
-            self.ui.textBrowser.append("Пожалуйста, прекратите!\nЭто совсем не смешно!")
-            return
-        elif -45<= counter < -25:
-            self.ui.textBrowser.append("Последнее предупреждение...")
-            return
-        elif counter == -46:
-            self.ui.textBrowser.append("СТОЙТЕ!!! НЕ НАЖИМАЙТЕ\nКНОПКУ!!!!")
-            return
-        elif counter<-46:
-            while True:
-                time.sleep(10000)
-            return
         for i in range(len(task)):
             if task[i].isdigit()==False and task[i]!=' 'and task[i]!='\n'and task[i]!='-':
                 self.ui.textBrowser.append("Некорректные данные!\nСимвольная ошибка")
+                output_error_log("Некорректные данные!\nСимвольная ошибка",self.ui.checkBox.isChecked(),self.path,self.save)
                 return
             if i<len(task)-1 and task[i].isdigit()  and task[i+1]=='-':
                 self.ui.textBrowser.append("Некорректные данные!\nСимвольная ошибка")
+                output_error_log("Некорректные данные!\nСимвольная ошибка",self.ui.checkBox.isChecked(),self.path,self.save)
                 return
             if (i<len(task)-1 and not task[i+1].isdigit()  and task[i]=='-' or
                     i==len(task)-1 and task[i]=='-'):
                 self.ui.textBrowser.append("Некорректные данные!\nСимвольная ошибка")
+                output_error_log("Некорректные данные!\nСимвольная ошибка",self.ui.checkBox.isChecked(),self.path,self.save)
                 return
         task_arr = [int(x) for x in task.split()]
         if len(task_arr) != (self.ui.spinBox_2.value() + 1) * (self.ui.spinBox.value() + 2):
             self.ui.textBrowser.append("Некорректные данные!\nОшибка количества\nвводных данных!")
+            output_error_log("Некорректные данные!\nОшибка количества\nвводных данных!",self.ui.checkBox.isChecked(),self.path,self.save)
             return
         P,D,A,b,symb = [],[],[],[],self.ui.comboBox.currentText()
         A_ras = []
@@ -94,52 +138,76 @@ class mywindow(QtWidgets.QMainWindow):
             count+=1
         if len(A) ==0:
             self.ui.textBrowser.append("Некорректные данные!\nОтсутствие ограничений!")
+            output_error_log("Некорректные данные!\nОтсутствие ограничений!",self.ui.checkBox.isChecked(),self.path,self.save)
             return
         if len(P) ==1:
             self.ui.textBrowser.append("Некорректные данные!\nОтсутствие переменных!")
+            output_error_log("Некорректные данные!\nОтсутствие переменных!",self.ui.checkBox.isChecked(),self.path,self.save)
             return
-        
+        if symb=='<' and inf_simplex(A,b)==False:
+            self.ui.textBrowser.append("Некорректные данные!\nСимплекс не ограничен!")
+            output_error_log("Некорректные данные!\nСимплекс не ограничен!",self.ui.checkBox.isChecked(),self.path,self.save)
+            return
+        if symb=='<' and double_phaz_sm(A,b)==False:
+            self.ui.textBrowser.append("Некорректные данные!\nСистема не совместна!")
+            output_error_log("Некорректные данные!\nСистема не совместна!",self.ui.checkBox.isChecked(),self.path,self.save)
+            return
+
         A_ras = copy.deepcopy(A_c)
         for i in range(len(A_ras)):
             A_ras[i] = np.append(A_ras[i],b_c[i])
         #Для совместности СЛАУ необходимо и достаточно, чтобы ранг её матрицы A был равен рангу её расширенной матрицы A.
         if np.linalg.matrix_rank(np.array(A_c))!=np.linalg.matrix_rank(np.array(A_ras)) and symb=='=':
             self.ui.textBrowser.append("Некорректные данные!\nСистема ограничений\nнесовместна!")
+            output_error_log("Некорректные данные!\nСистема ограничений\nнесовместна!",self.ui.checkBox.isChecked(),self.path,self.save)
             return
-        if np.linalg.matrix_rank(np.array(A_c))!=np.linalg.matrix_rank(np.array(A_ras)) and symb=='<':
-            self.ui.textBrowser.append("Некорректные данные!\nВозможно зацикливание\n")
-            return
+        #if np.linalg.matrix_rank(np.array(A_c))!=np.linalg.matrix_rank(np.array(A_ras)) and symb=='<':
+            #self.ui.textBrowser.append("Некорректные данные!\nВозможно зацикливание\n")
+            #return
         one_massive = [0]*len(D)
         one_massive[-1]=1
         try:
-            test1 = alg_dinkelbach1(np.array(D),np.array(one_massive),np.array(A),np.array(b),
-                               self.ui.comboBox.currentText(),False,False)
+            test1 = alg_dinkelbach_with_no_output(np.array(D),np.array(one_massive),np.array(A),np.array(b),
+                               symb,False,False,self.path,self.save)
+            test2 = alg_dinkelbach_with_no_output(np.array(D), np.array(one_massive), np.array(A), np.array(b),
+                                   symb, False, True, self.path, self.save)
         except:
-            self.ui.textBrowser.append("Ошибка\nВеликая ошибка...")
+            self.ui.textBrowser.append("Некорректные данные!\nНекорректная задача")
             return
-
-        
-        test2 = alg_dinkelbach1(np.array(D), np.array(one_massive), np.array(A), np.array(b),
-                               self.ui.comboBox.currentText(), False, True)
-        print(test1, test2)
-
-        def im_sorry(x):
-            sum = 0
-            for i in range(len(D) - 1):
-                sum += D[i] * x[i]
-            return sum + D[len(D) - 1]
-        if im_sorry(test1)>=0>=im_sorry(test2):
+        if test1[1]*test2[1]<=0:
             self.ui.textBrowser.append("Некорректные данные!\nЗнаменатель может\nпринять ноль!")
+            output_error_log("Некорректные данные!\nЗнаменатель может\nпринять ноль!", self.ui.checkBox.isChecked(),
+                             self.path, self.save)
             return
-        if im_sorry(test1) < 0:
-            rev(D)
-            mission = not mission
-        solution = alg_dinkelbach(np.array(P),np.array(D),np.array(A),np.array(b),symb,self.ui.checkBox.isChecked(),self.ui.checkBox_2.isChecked(),self.path,self.save)
+        if test1[1]>0:
+            neg=1
+        else:
+            neg=-1
+        #test2 = alg_dinkelbach1(np.array(D), np.array(one_massive), np.array(A), np.array(b),
+                               #self.ui.comboBox.currentText(), False, True)
+        #print(test1, test2)
 
-        for i in range(len(solution)):
-            a = 'X'+str(i)+' = '+str(solution[i])+' '
+        #def im_sorry(x):
+        #    sum = 0
+        #    for i in range(len(D) - 1):
+        #        sum += D[i] * x[i]
+        #    return sum + D[len(D) - 1]
+        #if im_sorry(test1)>=0>=im_sorry(test2):
+        #    self.ui.textBrowser.append("Некорректные данные!\nЗнаменатель может\nпринять ноль!")
+        #    output_error_log("Некорректные данные!\nЗнаменатель может\nпринять ноль!",self.ui.checkBox.isChecked(),self.path)
+        #    return
+        #if im_sorry(test1) < 0:
+        #    rev(D)
+        #    mission = not mission
+        if self.path =='':self.path = '.'
+
+        solution = alg_dinkelbach(np.array(P),np.array(D),np.array(A),np.array(b),symb,self.ui.checkBox.isChecked(),self.ui.checkBox_2.isChecked(),self.path,self.save, neg)
+
+        for i in range(len(solution[0])):
+            a = 'X'+str(i+1)+' = '+str(solution[0][i])+' '
             self.ui.textBrowser.append(a)
-
+        bb = 'Q(x*) = '+str(solution[1])
+        self.ui.textBrowser.append(bb)
 app = QtWidgets.QApplication([])
 application = mywindow()
 application.show()
--- main_page.py
+++ main_page.py
@@ -43,12 +43,12 @@ class Ui_Dialog(object):
         self.checkBox_2.setGeometry(QtCore.QRect(20, 110, 171, 20))
         self.checkBox_2.setObjectName("checkBox_2")
         self.comboBox = QtWidgets.QComboBox(Dialog)
-        self.comboBox.setGeometry(QtCore.QRect(20, 135, 16, 16))
+        self.comboBox.setGeometry(QtCore.QRect(20, 135, 35, 16))
         self.comboBox.setObjectName("comboBox")
         self.comboBox.addItem('=')
         self.comboBox.addItem('<')
         self.label_3 = QtWidgets.QLabel(Dialog)
-        self.label_3.setGeometry(QtCore.QRect(42, 135, 121, 16))
+        self.label_3.setGeometry(QtCore.QRect(60, 135, 121, 16))
         self.label_3.setObjectName("label_3")
 
         self.pushButton2 = QtWidgets.QPushButton(Dialog)
@@ -66,7 +66,7 @@ class Ui_Dialog(object):
         self.pushButton.setText(_translate("Dialog", "Решить"))
         self.pushButton2.setText(_translate("Dialog", "Путь txt"))
         self.checkBox.setText(_translate("Dialog", "Сохранить итерации в txt"))
-        self.checkBox_2.setText(_translate("Dialog", "Минимазация функции"))
+        self.checkBox_2.setText(_translate("Dialog", "Минимизация функции"))
         self.label_3.setText(_translate("Dialog", "Знак ограничений"))
 
     
